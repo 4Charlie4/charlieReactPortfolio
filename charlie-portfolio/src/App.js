b526b05cf78e738ecfaba81a90988b8bc74e64c1
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-10-26 22:07:27 -0400

    reinit files

--- charlie-portfolio/src/App.js
+++ charlie-portfolio/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div> </div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
---
+++ charlie-portfolio/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <a href="/">
+        <span>Charles Duran</span>
+      </a>
+    </header>
+  );
+}
+
+export default Header;
